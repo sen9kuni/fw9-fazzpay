ad3e318c319707c67b242541ca2e80a283872f8d
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-08-14 23:30:05 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,15 @@ import '../styles/style.css'
 import '../styles/login.css'
 import '../styles/home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
